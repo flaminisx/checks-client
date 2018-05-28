9a6fdabc369743023a9f52be56420e67cd93ee10
Igor Onyshchenko <optimum.flaminis@gmail.com>
2018-05-29 01:55:06 +0300

    added searchbar that does nothing

--- src/js/components/layouts/Header.js
+++ src/js/components/layouts/Header.js
@@ -5,17 +5,26 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import SearchBar from 'material-ui-search-bar'
 import { signOut as signOutAction } from '../../actions/userActions';
-
 
 const Header = (props) => (
    <AppBar position="static">
      <Toolbar>
        <Typography variant="title" color="inherit">Checks</Typography>
+       <SearchBar
+          onChange={() => console.log('onChange')}
+          onRequestSearch={() => console.log('onRequestSearch')}
+          style={{
+            marginRight: '0',
+            marginLeft: 'auto',
+            maxWidth: 800
+          }}
+        />
        <IconButton
           onClick={() => props.signOut()}
           color="inherit"
-          style={{marginLeft: 'auto'}}
+          style={{marginLeft: '20px'}}
         >
           <ExitToAppIcon/>
         </IconButton>
